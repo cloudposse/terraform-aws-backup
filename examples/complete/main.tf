e49db9e7248613a34f20cee2403235e8b512a75b
aknysh <andriy.knysh@gmail.com>
2020-09-09 13:23:13 -0400

    Update to `context.tf`. Update provider pinning. Update Terratest. Update GitHub Actions

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -26,11 +26,10 @@ module "subnets" {
 module "efs" {
   source = "git::https://github.com/cloudposse/terraform-aws-efs.git?ref=tags/0.19.0"
 
-  region             = var.region
-  availability_zones = var.availability_zones
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.subnets.private_subnet_ids
-  security_groups    = [module.vpc.vpc_default_security_group_id]
+  region          = var.region
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.subnets.private_subnet_ids
+  security_groups = [module.vpc.vpc_default_security_group_id]
 
   context = module.this.context
 }
