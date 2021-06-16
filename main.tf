85a4734fd9103f5f160140eb685b374bff579526
Benjamin Smith <ben.smith.developer@gmail.com>
2021-06-16 15:10:36 -0700

    allows easier creation of many of these modules

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ module "label_backup_role" {
 }
 
 resource "aws_backup_vault" "default" {
-  count       = module.this.enabled ? 1 : 0
+  count       = module.this.enabled && var.vault_enabled? 1 : 0
   name        = module.this.id
   kms_key_arn = var.kms_key_arn
   tags        = module.this.tags
 }
 
 resource "aws_backup_plan" "default" {
-  count = module.this.enabled ? 1 : 0
-  name  = module.this.id
+  count = module.this.enabled && var.plan_enabled ? 1 : 0
+  name  = var.plan_name_suffix == "" ? module.this.id : format("%s_%s", module.this.id, var.plan_name_suffix)
 
   rule {
     rule_name           = module.this.id
-    target_vault_name   = join("", aws_backup_vault.default.*.name)
+    target_vault_name   = var.target_vault_name == "" ? join("", aws_backup_vault.default.*.name) : var.target_vault_name
     schedule            = var.schedule
     start_window        = var.start_window
     completion_window   = var.completion_window
@@ -68,14 +68,19 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "default" {
-  count              = module.this.enabled ? 1 : 0
-  name               = module.label_backup_role.id
+  count              = module.this.enabled && var.iam_enabled ? 1 : 0
+  name               = var.target_iam_name == "" ? module.label_backup_role.id : var.target_iam_name
   assume_role_policy = join("", data.aws_iam_policy_document.assume_role.*.json)
   tags               = module.label_backup_role.tags
 }
 
+data "aws_iam_role" "existing" {
+  count              = module.this.enabled && var.iam_enabled ? 0 : 1
+  name = module.label_backup_role.id
+}
+
 resource "aws_iam_role_policy_attachment" "default" {
-  count      = module.this.enabled ? 1 : 0
+  count      = module.this.enabled && var.iam_enabled ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
   role       = join("", aws_iam_role.default.*.name)
 }
@@ -83,7 +88,7 @@ resource "aws_iam_role_policy_attachment" "default" {
 resource "aws_backup_selection" "default" {
   count        = module.this.enabled ? 1 : 0
   name         = module.this.id
-  iam_role_arn = join("", aws_iam_role.default.*.arn)
+  iam_role_arn = join("", var.iam_enabled ? aws_iam_role.default.*.arn : data.aws_iam_role.existing.*.arn)
   plan_id      = join("", aws_backup_plan.default.*.id)
   resources    = var.backup_resources
   dynamic "selection_tag" {
