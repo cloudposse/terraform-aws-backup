d9e1067291abccedb7194aa61b598a3a7898a79c
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-18 10:37:45 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,6 @@ output "backup_selection_id" {
   description = "Backup Selection ID"
 }
 
-output "backup_selection_id" {
-  value       = join("", aws_backup_selection.default.*.id)
-  description = "Backup Selection ID"
-}
-
 output "role_name" {
   value       = local.iam_role_name
   description = "The name of the IAM role created"
