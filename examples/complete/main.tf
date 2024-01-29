b044b8f0a5c80fac834fb7ab089093ea3bb20f27
Benjamin Smith <ben.smith.developer@gmail.com>
2024-01-29 09:49:22 -0800

    spacing fix

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -49,7 +49,7 @@ module "backup" {
       schedule          = var.schedule
       start_window      = var.start_window
       completion_window = var.completion_window
-      lifecycle         = {
+      lifecycle = {
         cold_storage_after = var.cold_storage_after
         delete_after       = var.delete_after
       }
