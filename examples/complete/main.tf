aeb53a9415a51fd41cb0d525ece40ad4da919261
Benjamin Smith <ben.smith.developer@gmail.com>
2024-01-29 09:58:37 -0800

    update subnets versions and terraform min version

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -13,13 +13,14 @@ module "vpc" {
 
 module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
-  version = "2.4.1"
+  version = "2.3.0"
 
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
   igw_id               = [module.vpc.igw_id]
   ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
-  nat_gateway_enabled  = false
+  max_nats             = 1
+  nat_gateway_enabled  = true
   nat_instance_enabled = false
 
   context = module.this.context
@@ -49,7 +50,7 @@ module "backup" {
       schedule          = var.schedule
       start_window      = var.start_window
       completion_window = var.completion_window
-      lifecycle = {
+      lifecycle         = {
         cold_storage_after = var.cold_storage_after
         delete_after       = var.delete_after
       }
